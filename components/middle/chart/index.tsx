34c7ca555e192fc2ed268dacaf3a264afee9e960
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-09-29 21:29:39 +0100

    refactor code

--- components/middle/chart/index.tsx
+++ components/middle/chart/index.tsx
@@ -1,39 +1,78 @@
-import { useEffect } from "react"
-import { chartData } from "@data/data"
+import { useEffect, useState } from "react";
+import { chartData } from "@data/data";
 import {
   Chart,
+  ArcElement,
   LineElement,
+  BarElement,
   PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
   LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
   CategoryScale,
   LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
   Legend,
+  Title,
   Tooltip,
-} from "chart.js"
+  SubTitle,
+} from "chart.js";
 
 Chart.register(
+  ArcElement,
   LineElement,
+  BarElement,
   PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
   LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
   CategoryScale,
   LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
   Legend,
-  Tooltip
-)
+  Title,
+  Tooltip,
+  SubTitle
+);
+
+import { chartType } from "@/types/types";
 
 export default function GrapChart() {
+  const [chart, setChart] = useState();
 
-useEffect(() => {
-  const canvas = document.getElementById("chart") as HTMLCanvasElement
-  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
-  // new Chart instance
-  new Chart(ctx, chartData as any)
-  })
+  useEffect(() => {
+    const canvas = document.getElementById("chart") as HTMLCanvasElement;
+    const graph = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    // new Chart instance
+    new Chart(graph, chartData as any);
+  });
   return (
     <>
       <canvas
-        className="!bg-colorWhite dark:!bg-colorWhite_DM !w-full mt-8 !rounded-3xl !p-6 !mb-6" id="chart"
+        className="!bg-colorWhite dark:!bg-colorWhite_DM !w-full mt-8 !rounded-3xl !p-6 !mb-6"
+        id="chart"
       />
     </>
-  )
+  );
 }
