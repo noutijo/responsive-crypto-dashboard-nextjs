141c656649a64b32cee417c942afbec3fe015145
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-09-29 21:48:43 +0100

    refactor code

--- pages/analytics/index.tsx
+++ pages/analytics/index.tsx
@@ -1,6 +1,5 @@
 import PrincipalLayout from "@components/layouts/PrincipalLayout"
 import PageTemplate from "@components/template/PageTemplate"
-
 export default function Analytics() {
   return (
     <>
