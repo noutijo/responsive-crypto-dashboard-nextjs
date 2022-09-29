b2cd6e5304c52d6e9423b9361df55da3b542e66d
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-09-29 22:32:16 +0100

    refactor code

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,12 +1,12 @@
-import PrincipalLayout from "@components/layouts/PrincipalLayout";
-import PageTemplate from "@components/template/PageTemplate";
+import PrincipalLayout from "@components/layouts/PrincipalLayout"
+import PageTemplate from "@components/template/PageTemplate"
 
 export default function Settings() {
   return (
     <>
       <PageTemplate title={"Settings"} />
     </>
-  );
+  )
 }
 
-Settings.Layout = PrincipalLayout;
+Settings.Layout = PrincipalLayout
