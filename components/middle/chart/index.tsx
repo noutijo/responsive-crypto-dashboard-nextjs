0c92225816b474b20cfe8021ee41c7bbc6caca8d
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-09-29 21:23:25 +0100

    refactor code

--- components/middle/chart/index.tsx
+++ components/middle/chart/index.tsx
@@ -2,66 +2,32 @@ import { useEffect } from "react"
 import { chartData } from "@data/data"
 import {
   Chart,
-  ArcElement,
   LineElement,
-  BarElement,
   PointElement,
-  BarController,
-  BubbleController,
-  DoughnutController,
   LineController,
-  PieController,
-  PolarAreaController,
-  RadarController,
-  ScatterController,
   CategoryScale,
   LinearScale,
-  LogarithmicScale,
-  RadialLinearScale,
-  TimeScale,
-  TimeSeriesScale,
-  Decimation,
-  Filler,
   Legend,
-  Title,
   Tooltip,
-  SubTitle,
-} from "chart.js";
+} from "chart.js"
 
 Chart.register(
-  ArcElement,
   LineElement,
-  BarElement,
   PointElement,
-  BarController,
-  BubbleController,
-  DoughnutController,
   LineController,
-  PieController,
-  PolarAreaController,
-  RadarController,
-  ScatterController,
   CategoryScale,
   LinearScale,
-  LogarithmicScale,
-  RadialLinearScale,
-  TimeScale,
-  TimeSeriesScale,
-  Decimation,
-  Filler,
   Legend,
-  Title,
-  Tooltip,
-  SubTitle
-);
+  Tooltip
+)
 
 export default function GrapChart() {
 
 useEffect(() => {
   const canvas = document.getElementById("chart") as HTMLCanvasElement
-  const graph = canvas.getContext("2d") as CanvasRenderingContext2D
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
   // new Chart instance
-  new Chart(graph, chartData as any)
+  new Chart(ctx, chartData as any)
   })
   return (
     <>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -17,7 +18,11 @@ export default function Document() {
       <body className="w-screen h-screen overflow-x-hidden select-none text-colorDark dark:text-colorDark_DM bg-colorLight dark:bg-colorLight_DM font-sans">
         <Main />
         <NextScript />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js"
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
-  )
+  );
 }
