f0d7cfea070e5c9cb2d036098ea7f7dae07d89d4
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-09-29 22:10:32 +0100

    refactor code

--- components/middle/chart/index.tsx
+++ components/middle/chart/index.tsx
@@ -1,72 +1,8 @@
 import { useEffect, useState } from "react";
 import { chartData } from "@data/data";
-import {
-  Chart,
-  ArcElement,
-  LineElement,
-  BarElement,
-  PointElement,
-  BarController,
-  BubbleController,
-  DoughnutController,
-  LineController,
-  PieController,
-  PolarAreaController,
-  RadarController,
-  ScatterController,
-  CategoryScale,
-  LinearScale,
-  LogarithmicScale,
-  RadialLinearScale,
-  TimeScale,
-  TimeSeriesScale,
-  Decimation,
-  Filler,
-  Legend,
-  Title,
-  Tooltip,
-  SubTitle,
-} from "chart.js";
-
-Chart.register(
-  ArcElement,
-  LineElement,
-  BarElement,
-  PointElement,
-  BarController,
-  BubbleController,
-  DoughnutController,
-  LineController,
-  PieController,
-  PolarAreaController,
-  RadarController,
-  ScatterController,
-  CategoryScale,
-  LinearScale,
-  LogarithmicScale,
-  RadialLinearScale,
-  TimeScale,
-  TimeSeriesScale,
-  Decimation,
-  Filler,
-  Legend,
-  Title,
-  Tooltip,
-  SubTitle
-);
-
-import { chartType } from "@/types/types";
 
 export default function GrapChart() {
-  const [chart, setChart] = useState();
 
-  useEffect(() => {
-    const canvas = document.getElementById("chart") as HTMLCanvasElement;
-    const graph = canvas.getContext("2d") as CanvasRenderingContext2D;
-
-    // new Chart instance
-    new Chart(graph, chartData as any);
-  });
   return (
     <>
       <canvas
